b3879055b587c35d7068680b014e2d3a2961c2fa
Toni <toni@Tonis-MBP.home>
2021-11-03 12:27:31 +0100

    up

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: antgonza <antgonza@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:10:30 by antgonza          #+#    #+#             */
-/*   Updated: 2021/11/03 08:26:25 by antgonza         ###   ########.fr       */
+/*   Updated: 2021/11/03 11:57:47 by antgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_cd(char *argv)
 	i = 0;
 	argv++;
 	argv++;
-	printf("ENTRAAAcd \n%s\n", argv);
+	printf("ENTRAAAcd%sSALE", argv);
 	chdir("..");
 	//chdir(argv);
 }
--- srcs/ft_prompt_cmp.c
+++ srcs/ft_prompt_cmp.c
@@ -15,22 +15,23 @@ void ft_prompt_cmp(char *prompt, char **envp)
 
 
 	i = 0;
-	if (ft_strcmp("pwd", prompt) == 0) //compara este string con lo que está en prompt (lo que metemos)
+	if (ft_strncmp("pwd", prompt, 3) == 0) //compara este string con lo que está en prompt (lo que metemos)
 				printf("%s\n", getcwd(NULL, 0));
-	else if (ft_strcmp("exit", prompt) == 0)
+	else if (ft_strncmp("exit", prompt, 4) == 0)
 	{
 		write(1, "exit\n", 5);
 		exit(1);
 	}
-	else if (ft_strcmp("echo", prompt) == 0)
+	else if (ft_strncmp("echo", prompt, 4) == 0)
 	{
 		ft_echo(argv);
 	}
-	else if (ft_strcmp("cd", prompt) == 0)
+	else if (ft_strncmp("cd", prompt, 2) == 0)
 	{
+		printf("ENTRAAAcd%sSALE", prompt);
 		ft_cd(prompt);
 	}
-	else if (ft_strcmp("export", prompt) == 0)
+	else if (ft_strncmp("export", prompt, 6) == 0)
 	{
 		i = 0;
 		while (environ[i] != NULL)
@@ -40,11 +41,11 @@ void ft_prompt_cmp(char *prompt, char **envp)
 			i++;
 		}
 	}
-	else if (ft_strcmp("unset", prompt) == 0)
+	else if (ft_strncmp("unset", prompt, 5) == 0)
 	{
 		printf("función unset\n");
 	}
-	else if (ft_strcmp("env", prompt) == 0)
+	else if (ft_strncmp("env", prompt, 3) == 0)
 	{
 		i = 0;
 		while (environ[i] != NULL)
