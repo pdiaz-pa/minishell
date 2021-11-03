c3bbc220f27a4ea6ad951daa53d4be80924d6989
Toni <toni@Tonis-MBP.home>
2021-11-03 08:13:32 +0100

    up

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: antgonza <antgonza@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:10:30 by antgonza          #+#    #+#             */
-/*   Updated: 2021/10/30 11:28:45 by antgonza         ###   ########.fr       */
+/*   Updated: 2021/11/03 08:12:13 by antgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	ft_cd()
 	size_t	i;
 	
 	i = 0;
-	getcwd("/bin/", i);
+	//chdir("..");
+	chdir("/usr/bin");
 }
