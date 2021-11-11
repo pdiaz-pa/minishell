2d150bcb5976543a173a3d8ddaa291e08ece982d
Pablo Diaz pastor <pdiaz-pa@c3r2s1.42madrid.com>
2021-11-11 10:12:48 +0100

    main actualizado

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,20 @@
 #include "../includes/minishell.h"
 
+int ft_only_spaces(char *prompt)
+{
+	int count;
+
+	count = 0;
+
+	while (prompt[count] != '\0')
+	{
+		if(prompt[count] != ' ' && prompt[count] != '\0')
+			return(1);
+		count++;
+	}
+	return(0);
+}
+
 void	ft_sig_int(int signal)
 {
 	if (signal == SIGINT)
@@ -11,7 +26,7 @@ void	ft_sig_int(int signal)
 	}
 }
 
-int	main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv/*, char **envp*/)
 {
 	/*Antes de todo, hay que hacer que:
 		-Control + C no salga del programa
@@ -34,9 +49,9 @@ int	main(int argc, char **argv, char **envp)
 			add_history(prompt); // añade la función de historia e implementa por sí mismo la funcionalidad de las flechas
 			if (prompt == NULL || ft_strcmp("exit", prompt) == 0) // hay que ponerlo primero por que si strcmp detecta el NULL devuelve 0 y entra en la funicon
 				exit(1);
-			ft_prompt_cmp(prompt, envp);
-			if (prompt[0] != '\0') // para evitar crasheo al no pasarle nada
-			//	ft_tokenizer(prompt); //en desarrollo. comentar esta  función si se quiere probar algo
+			//ft_prompt_cmp(prompt, envp);
+			if (prompt[0] != '\0' && ft_only_spaces(prompt) == 1) // para evitar crasheo al no pasarle nada o solo espacios
+				ft_tokenizer(prompt); //en desarrollo. comentar esta  función si se quiere probar algo
 			free(prompt);
 		}
 	}
