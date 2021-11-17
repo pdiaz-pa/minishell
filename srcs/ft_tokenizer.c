82a13249c26e74aff91927ca271a0238dad717a7
Pablo Diaz pastor <pdiaz-pa@c3r2s1.42madrid.com>
2021-11-17 16:27:42 +0100

    tokenizer funcional

--- srcs/ft_tokenizer.c
+++ srcs/ft_tokenizer.c
@@ -1,23 +1,25 @@
 #include "../includes/minishell.h"
 
+
 int ft_doubleq_mode(char *prompt, t_tokenizer *tk)
 {
 	int size;
 	size = 0;
 	tk->start++;
 	//tk->sizer++;
-	printf("\e[0;31mMODO DOBLE\e[0m\n");
+	//tk->double_flag = 1;
+	//printf("\e[0;31mMODO DOBLE\e[0m\n");
 	while (prompt[tk->start] != 34 && prompt[tk->start] != '\0')
 	{
-		printf("%c CHAR ", prompt[tk->start]);
+		//printf("%c CHAR ", prompt[tk->start]);
 		tk->start++;
 		size++;
-		printf("%d START double\n", tk->start);
+		//printf("%d START double\n", tk->start);
 	}
 	if (prompt[tk->start] == 34) 
 	{
 		tk->start++;
-		tk->sizer++;
+		//tk->sizer++;		
 	}
 	return(size);
 }
@@ -25,22 +27,24 @@ int ft_doubleq_mode(char *prompt, t_tokenizer *tk)
 int ft_singleq_mode(char *prompt, t_tokenizer *tk)
 {
 	tk->start++;
-	//tk->sizer++;
+	//tk->sizer++;	
+	tk->sizer++;
 	int size;
 
 	size = 0;
-	printf("\e[0;33mMODO SINGLE\e[0m\n");
+	//printf("\e[0;33mMODO SINGLE\e[0m\n");
+	tk->single_flag = 1;
 	while (prompt[tk->start] != 39 && prompt[tk->start] != '\0')
 	{
-		printf("%c CHAR ", prompt[tk->start]);
+		//printf("%c CHAR ", prompt[tk->start]);
 		tk->start++;
 		size++;
-		printf("%d START single\n", tk->start);
+		//printf("%d START single\n", tk->start);
 	}
 	if (prompt[tk->start] == 39)
 	{
 		tk->start++;
-		tk->sizer++;
+		//tk->sizer++;
 	}
 	return(size);
 }
@@ -49,15 +53,15 @@ int ft_normal_mode(char *prompt, t_tokenizer *tk)
 {
 	int size;
 	size = 0;
-	printf("\e[1;34mMODO NORMAL\e[0m\n");
+	//printf("\e[1;34mMODO NORMAL\e[0m\n");
 	while(prompt[tk->start] != ' ' && prompt[tk->start] != '\0')
 	{
-		printf("%c CHAR ", prompt[tk->start]);
+		//printf("%c CHAR ", prompt[tk->start]);
 		if (prompt[tk->start] != 34 && prompt[tk->start] != 39)
 		{
 			size++;
 			tk->start++;
-			printf("%d START \n", tk->start);
+			//printf("%d START \n", tk->start);
 		}
 		else
 		{
@@ -67,11 +71,11 @@ int ft_normal_mode(char *prompt, t_tokenizer *tk)
 				size = size + (ft_doubleq_mode(prompt, tk));
 		}	
 	}
-	printf("%d SIZE DEL STRING\n", size);
+	//printf("%d SIZE DEL STRING\n", size);
 	if (prompt[tk->start] == ' ' || prompt[tk->start] == '\0')
 	{
 		tk->size++;
-		printf("%d +1 SIZE NORMAL\n", tk->size);
+		//printf("%d +1 SIZE NORMAL\n", tk->size);
 	}
 	return (size);
 }
@@ -103,36 +107,61 @@ int ft_str_size(char *prompt, t_tokenizer *tk, t_list *token_list)
 	char *buff;
 	buff = NULL;
 	j = 0;
-	printf("\e[42mCALCUATING *CHAR's SIZE...\e[0m\n");
+	//printf("\e[42mCALCUATING *CHAR's SIZE...\e[0m\n");
 	while (prompt[tk->start] != '\0')
 	{
 		while (prompt[tk->start] == ' ')
 		{
 			tk->start++;
 			tk->sizer++;
-			printf("%d SKIPPING SPACE\n", tk->start);
+			//printf("%d SKIPPING SPACE\n", tk->start);
 		}
 		if(ft_last_spaces(prompt, tk) == 1)
 		{
 			ft_normal_mode(prompt, tk);
-			printf("%d STRING SIZE \n", (tk->start - tk->sizer));
+			//printf("%d STRING SIZE \n", (tk->start - tk->sizer));
 			buff = malloc((tk->start - tk->sizer) + 1);
 			while (tk->sizer < tk->start)
 			{
-				printf("%d tkstart  %d  tksizer  %c\n", tk->start, tk->sizer, prompt[tk->sizer]);
-				buff[j] = prompt[tk->sizer];
-				j++;
-				tk->sizer++;
+				//printf("%d tkstart  %d  tksizer  %c\n", tk->start, tk->sizer, prompt[tk->sizer]);
+				if (tk->double_flag == 1 && prompt[tk->sizer] == 34)
+				{
+					tk->sizer++;
+					tk->double_flag = 0;
+				}
+				else if (tk->double_flag == 0 && prompt[tk->sizer] == 34 && tk->single_flag == 0)
+				{
+					tk->sizer++;
+					tk->double_flag = 1;
+				}
+				else if (tk->single_flag == 1 && prompt[tk->sizer] == 39)
+				{
+					tk->sizer++;
+					tk->single_flag = 0;
+				}
+				else if (tk->single_flag == 0 && prompt[tk->sizer] == 39 && tk->double_flag == 0)
+				{
+					tk->sizer++;
+					tk->single_flag = 1;
+				}
+				else
+				{
+					buff[j] = prompt[tk->sizer];
+					j++;
+					tk->sizer++;
+				}
+
 			}
 			buff[j] = '\0';
-			printf("arr---->%s\n", buff);
+			//printf("buffer---->%s\n", buff);
 			ft_lstadd_back(&token_list, ft_lstnew(buff));
+			//ft_stack_printer(token_list);
 			j = 0;
-			free(buff);
+			buff = NULL;
 		}
 	}
-	printf("\e[42m--------ENDED. TK->SIZE: \e[0m");
-	printf("\e[42m %d --------\e[0m\n\n", tk->size);
+	//printf("\e[42m--------ENDED. TK->SIZE: \e[0m");
+	//printf("\e[42m %d --------\e[0m\n\n", tk->size);
 	return (tk->size);
 }
 
