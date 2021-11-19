69f60aa505f0b875496ac2c4c5caf7419779fb10
Pablo Diaz pastor <pdiaz-pa@c3r2s1.42madrid.com>
2021-11-19 16:40:02 +0100

    list utils

--- srcs/ft_list_utils.c
+++ srcs/ft_list_utils.c
@@ -1,78 +1,15 @@
 #include "../includes/minishell.h"
-/*
-void	ft_free_all(t_mylist *token_list, char **token_arr, t_tokenizer *tk)
+
+t_mylist	*ft_mylstlast(t_mylist *lst)
 {
-	int i;
-
-	i = 0;
-
-	while (i != tk->size)
-	{
-		free(token_arr[i]);
-		i++;
-	}
-	free(token_arr);
-	ft_free_list(token_list);
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
 
-void	ft_free_list(t_mylist *token_list)
-{
-	t_mylist	*ntoken;
-
-	while (token_list)
-	{
-		ntoken = token_list->next;
-		free(token_list);
-		token_list->next = NULL;
-		token_list->prev = NULL;
-		token_list = ntoken;
-	}
-}
-
-
-
-t_mylist *ft_init_t_stack(void)
-{
-    t_mylist *stack;
-    stack = (t_mylist *)malloc(sizeof(t_mylist));
-    stack->prev = NULL;
-    stack->next = NULL;
-    stack->token = NULL;
-
-    return(stack);
-}
-
-void ft_make_list(t_mylist *head, char **token_arr, int array_size)
-{
-	int		idx;
-	t_mylist	*new_node;
-
-	idx = 0;
-	while (idx < array_size)
-	{
-		new_node = (t_mylist *)malloc(sizeof(t_mylist));
-    
-		if (!new_node)
-			printf("fuckyou");
-		if (head->next == NULL)
-		{
-			new_node->token = token_arr[idx++];
-			new_node->next = NULL;
-			new_node->prev = head;
-			head->next = new_node;
-		}
-		else
-		{
-			new_node->token = token_arr[idx++];
-			new_node->next = head->next;
-			new_node->next->prev = new_node;
-			new_node->prev = head;
-			head->next = new_node;
-		}
-	}
-}
-*/
-void ft_stack_printer(t_list *stack)
+void ft_stack_printer(t_mylist *stack)
 {
     stack = stack->next; // para que no se imprima el primer 0 (head)
     while(stack != NULL) // imprime cada nodo 
@@ -81,4 +18,34 @@ void ft_stack_printer(t_list *stack)
         stack = stack->next;
     }
     printf("NULL\n");
+}
+
+void	ft_mylstadd_back(t_mylist **lst, t_mylist *new)
+{
+	t_mylist	*aux;
+
+	if (new)
+	{
+		if (!*lst)
+		{
+			*lst = new;
+			return ;
+		}
+		aux = *lst;
+		aux = ft_mylstlast(aux);
+		aux->next = new;
+	}
+	return ;
+}
+
+t_mylist	*ft_mylstnew(void *content)
+{
+	t_mylist	*new;
+
+	new = malloc(sizeof(t_mylist));
+	if (new == NULL)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
 }
