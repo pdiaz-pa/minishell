504a7ba4ca7d927ae39866f83b23f81f71056dfd
Toni <toni@MacBook-Pro.local>
2021-11-27 08:36:13 +0100

    UP

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: antgonza <antgonza@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:10:30 by antgonza          #+#    #+#             */
-/*   Updated: 2021/11/26 08:44:04 by antgonza         ###   ########.fr       */
+/*   Updated: 2021/11/26 18:41:38 by antgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,40 @@ Primero necesito hacer env para actualizar PWD con el nuevo dir
  */
 void	ft_cd(t_env **env, t_mylist *tk_l)
 {
-	if (env){}
-	if (tk_l){}
-	
-	/* int	i;
 	t_env	*temp;
-	t_env	*temp2;
+	char	ret;
 
-	i = 0;
-	if (argv[0] == 0 || argv[1] ==  '~')
+	ret = '0';
+	if (tk_l == NULL /* || (ft_strcmp(tk_l->content, "~")) == 0 */)
 	{
-		temp = ft_search_env(env, "HOME");
-		chdir(temp->line[1]);
-	
+		temp = ft_search_env(*env, "HOME");
+		printf("\ntemp = %p\n", temp);
+		if (temp == NULL)
+		{
+			printf("minishell: cd: HOME not set\n");
+			ret = '1';
+		}
+		else
+		{
+			if((chdir(temp->line[1])) == -1)
+			{
+				printf("minishell: cd: %s: No such file or directory\n", temp->line[1]);
+				ret = '1';
+			}
+		}
 	}
-	argv++;
-	chdir(argv);
-	temp = ft_search_env(env, "OLDPWD");
+	else 
+	{
+		if ((chdir(tk_l->content)) == -1)
+		{
+			printf("minishell: cd: %s: No such file or directory\n", tk_l->content);
+			ret = '1';
+		}
+	}
+	if (ret == '0')
+	{}
+	return ;
+	/* temp = ft_search_env(env, "OLDPWD");
 	temp2 = ft_search_env(env, "PWD");
 	free (temp->line[1]);
 	temp->line[1] = temp2->line[1];
--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,19 +6,20 @@
 /*   By: antgonza <antgonza@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 17:50:14 by antgonza          #+#    #+#             */
-/*   Updated: 2021/11/25 18:16:48 by antgonza         ###   ########.fr       */
+/*   Updated: 2021/11/26 12:03:41 by antgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static void	ft_unset_2(t_env **env, t_env *unset);
+
 int	    ft_unset(t_env **env, t_mylist *tk_l)
 {
-	t_env		*temp;
+	t_env		*unset;
 	t_mylist	*list;
 	char		ret;
 
-	temp = *env;
 	ret = '0';
 	if (tk_l == NULL || tk_l->tk_type == 1)
 		return 0;
@@ -30,9 +31,32 @@ int	    ft_unset(t_env **env, t_mylist *tk_l)
 			printf("minishell: unset: `%s': not a valid identifier\n", tk_l->content);
 			ret = '1';
 		}
+		unset = ft_search_env(*env, list->content);
+		if (unset != NULL)
+			ft_unset_2(env, unset);
 		list = list->next;
 	}
 	if (ret == '1')
 		return (1);
 	return (0);
+}
+
+static void	ft_unset_2(t_env **env, t_env *unset)
+{
+	t_env	*temp;
+
+	temp = *env;
+	if (temp == unset)
+		*env = temp->next;
+	else
+	{
+		while (temp->next != unset)
+			temp = temp->next;
+		temp->next = unset->next;
+	}
+	free (unset->line[0]);
+	if (unset->line[1] != NULL)
+		free (unset->line[1]);
+	free (unset);
+	return ;
 }
