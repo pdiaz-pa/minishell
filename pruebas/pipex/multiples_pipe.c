af4759e098d951abddbb33c9d560583d6ca9d14b
Antonio Gonzalez pozo <antgonza@c1r16s6.42madrid.com>
2021-12-02 18:39:45 +0100

    ruebas

--- pruebas/pipex/multiples_pipe.c
+++ pruebas/pipex/multiples_pipe.c
@@ -1,73 +1,76 @@
-/*
- * 		** fork y pipe**
- *
- *	1. Pipe
- *		- Mecanismo IPC unidireccional.
- *		- Comunica procesos relacionados.
- *		- Envios de "byte streams"
- *		- pipe asociado a dos fd.
- *	2. Funciones
- *		- Crear pipe: int pipe(int fd[2]); fd[0] READ, fd[1] WRITE
- *		- Escritura: ssize_t write(int fd, const void, *buf, size_t nr);
- *		- Lectura: ssize_t read(int fd, void *buf, size_t nr);
- *		- Cerrar: int close(int fd);
-*/
-
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
 
-void	ft_fork(char *argv, int	*fd, int num)
+#define READ_END    0    /* index pipe extremo escritura */
+#define WRITE_END   1    /* index pipe extremo lectura */
+
+int main(int argc, char* argv[])   
 {
-	pid_t p1;  // Argumento para fork. 
-	char buf[600]; // Aqui guardaremos lo leido del fd.
-	int	n_read; // En esta variable guardamos los bytes leidos con read.
+    int fd1[2],fd2[2];
+    int status, pid;
+	
+    pipe(fd1);                 /* comunica ls y grep */
+	
+    pid = fork();     
 
-	p1 = fork(); // Creamos el proceso hijo.
-	if (p1 == -1) // Si falla fork.
-		printf("Error\n");
-	else if (p1 > 0) // Se esta ejecutando el padre
-	{
-		close(fd[1]); // Cerramos el extremo de escritura del pipe.
-		n_read = read(fd[0], buf, sizeof(buf) - 1); //Read no pone \0 
-		close(fd[0]); // Una vez leido cerramos el fd.
-		buf[n_read] = '\0'; // Importantisimo cerrar el str.
-		printf("Se han leido %d bytes: %s\n",n_read, buf);
-		exit(0);
+    if(pid == 0)              /* hijo 1*/
+    {      		
+        close(fd1[READ_END]);   /* cerrar extremo no necesario */
+		
+        dup2(fd1[WRITE_END], STDOUT_FILENO); 
+	close(fd1[WRITE_END]);
+		
+        execlp("/bin/ls", "ls", "-l", NULL);
+    }
+    else                          /* padre */
+    { 
+	close(fd1[WRITE_END]);    /* extremo no necesario ya */
+		
+	pipe(fd2);		  /* comunica grep y wc */
+        pid = fork();
+        
+       if(pid == 0)               /* hijo 2 */
+       {
+	  close(fd2[READ_END]);   /* cerrar extremo no necesario */
+									
+          dup2(fd1[READ_END], STDIN_FILENO);
+	  close(fd1[READ_END]);
+			
+	  dup2(fd2[WRITE_END], STDOUT_FILENO);			
+	  close(fd2[WRITE_END]);
+			
+          execlp("/usr/bin/grep","grep", "u",NULL);
 	}
-	else if (p1 == 0) //Se esta ejecutando el hijo
+	else /* padre */
 	{
-		close(fd[0]); // Cerramos el extremo de lectura del pipe.
-		write(fd[1], argv, 5); // Escribimos en el extremo de escritura.
-		close(fd[1]); // Una vez escrito podemos cerrar el fd de escritura.
-		exit(0);  // Terminamos el proceso hijo.
-	}
+           close(fd1[READ_END]);      /* cerrar extremo no necesario */
+           close(fd2[WRITE_END]);     /* cerrar extremo no necesario */
+			
+     	   pid = fork();
+		
+	   if(pid == 0) /* hijo 3*/
+          {
+	    dup2(fd2[READ_END], STDIN_FILENO);
+	    close(fd2[READ_END]);
+				
+	    execlp("/usr/bin/wc","wc", "-l",NULL);
+	  }
+        }		        
+      }
+    
+    close(fd2[READ_END]);  /* cerrar extremo que queda abierto en el padre  */
+	
+   /* wait para cada hijo */
+    wait(&status);   
+    wait(&status);	
+    wait(&status);
+	
+    return 0;
+  	
 }
+	
 
-void	ft_pipes(int total, char **argv)
-{
-	int i;
-	int fd[total][2]; // Este fd se enviara como argumento a pipe.
-
-	i = 0;
-	while (i < total)
-	{
-		pipe(fd[i]);
-		i++;
-	}
-	i = 0;
-	while (i < total)
-	{
-		ft_fork(argv[i + 1], fd[i], i);
-		i++;
-	}
-
-
-}
-
-int main(int argc, char **argv)
-{
-	ft_pipes(argc - 1, argv);
-
-	return (0);
-}
