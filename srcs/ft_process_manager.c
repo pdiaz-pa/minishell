8fd05f03c33e1d3bb2dc389a74c596b9583dc232
Toni <toni@DESKTOP-7OVU3K3.home>
2021-12-16 22:20:42 +0100

    pipes 99%

--- srcs/ft_process_manager.c
+++ srcs/ft_process_manager.c
@@ -6,7 +6,7 @@
 /*   By: antgonza <antgonza@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 11:59:54 by antgonza          #+#    #+#             */
-/*   Updated: 2021/12/16 21:39:33 by antgonza         ###   ########.fr       */
+/*   Updated: 2021/12/16 22:19:19 by antgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,17 +68,55 @@ static void	ft_first_process(t_env *env, t_proc *process)
 	else
 	{
 		close(process->fd[1]);
+		//process->pid = waitpid(-1, &process->ret, 0);
 	}
 
 
 }
 static void	ft_intermediate_process(t_env *env, t_proc *process)
 {
-	if (env){}
-	if (process){}
+	pipe(process->fd);
+	process->pid = fork();
+	if (process->pid == 0)
+	{
+		close (process->fd[0]);
+		dup2(process->prev->fd[0], STDIN_FILENO);
+		close(process->prev->fd[0]);
+		dup2(process->fd[1], STDOUT_FILENO);
+		close(process->fd[1]);
+		if (process->list != NULL)
+			ft_prompt_cmp(env, process->list, 'b');
+	}
+	else
+	{
+		close(process->prev->fd[0]);
+		close(process->fd[1]);
+		//process->pid = waitpid(-1, &process->ret, 0);
+	}
+
 }
 static void	ft_last_process(t_env *env, t_proc *process)
 {
-	if (env){}
-	if (process){}
+	int	i;
+
+	i = 0;
+	ft_redir_out(process);
+	process->pid = fork();
+	if (process->pid == 0)
+	{
+		dup2(process->prev->fd[0], STDIN_FILENO);
+		close(process->prev->fd[0]);
+		if (process->list != NULL)
+			ft_prompt_cmp(env, process->list, 'b');
+	}
+	else
+	{
+		close(process->prev->fd[0]);
+		while(i < process->total)
+		{
+		process->pid = waitpid(-1, &process->ret, 0);
+		i++;
+		}
+	}
+	
 }
