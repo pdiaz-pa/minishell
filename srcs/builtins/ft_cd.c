0fe88d53ce0f9498d83fe0cb7cbcf242cc9b4ef9
Toni <toni@DESKTOP-7OVU3K3.home>
2021-12-17 21:54:53 +0100

    check cd echo

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: antgonza <antgonza@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:10:30 by antgonza          #+#    #+#             */
-/*   Updated: 2021/12/11 11:53:27 by antgonza         ###   ########.fr       */
+/*   Updated: 2021/12/17 21:41:01 by antgonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,22 @@
 static int	ft_cd_home(t_env **env);
 static void	ft_update_pwd(t_env **env, char *old);
 
-int	ft_cd(t_env **env,  t_cont *command)
+int	ft_cd(t_env **env, t_cont *command)
 {
 	int		ret;
 	char	*old;
 
 	ret = 0;
 	old = getcwd(NULL, 0);
-	if (command == NULL /* || (ft_strcmp(tk_l->content, "~")) == 0 */)
+	if (command == NULL)
 		ret = ft_cd_home(env);
-	else 
+	else
 	{
 		if ((chdir(command->content)) == -1)
 		{
-			printf("minishell: cd: %s: No such file or directory\n",
-				command->content);
+			ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+			ft_putstr_fd(command->content, STDERR_FILENO);
+			ft_putendl_fd(": No such file or directory", STDERR_FILENO);
 			ret++;
 		}
 	}
@@ -46,15 +47,16 @@ static int	ft_cd_home(t_env **env)
 	temp = ft_search_env(*env, "HOME");
 	if (temp == NULL)
 	{
-		printf("minishell: cd: HOME not set\n");
+		ft_putstr_fd("minishell: cd: HOME not set\n", STDERR_FILENO);
 		return (1);
 	}
 	else
 	{
-		if((chdir(temp->line[1])) == -1)
+		if ((chdir(temp->line[1])) == -1)
 		{
-			printf("minishell: cd: %s: No such file or directory\n",
-				temp->line[1]);
+			ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+			ft_putstr_fd(temp->line[1], STDERR_FILENO);
+			ft_putendl_fd(": No such file or directory", STDERR_FILENO);
 			return (1);
 		}
 	}
