ba33e9e4564e37056ba671274674d8094ed459c8
Toni <toni@MacBook-Pro.local>
2021-12-30 12:00:27 +0100

    exit values 55%

--- srcs/ft_heredoc.c
+++ srcs/ft_heredoc.c
@@ -11,6 +11,8 @@ void	ft_sig_int_here(int signal)
 	}
 }
 
+/*
+
 int ft_heredoc(t_proc *process)
 {
     //SIGNALS???
@@ -36,4 +38,46 @@ int ft_heredoc(t_proc *process)
     }
     close (fd);
     return(0);
+}
+
+*/
+
+int ft_heredoc(t_proc *process) // con proceso hijo
+{
+    //SIGNALS???
+    char *keyword;
+    int again;
+    int fd;
+	pid_t	pid;
+
+	pid = fork();
+    if (pid == -1)
+		perror("pid error");
+	else if (pid == 0) // todo dentro de este else es el hijo
+    {
+        again = 1;
+        fd = open(".heredoc", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+        signal(SIGINT, SIG_IGN);
+        signal(SIGINT, ft_sig_int_here);
+        while (again)
+        {
+            keyword = readline("> ");
+            if (ft_strcmp(keyword, process->input) == 0)
+                again = 0;
+            else
+            {
+                write(fd, keyword, ft_strlen(keyword));
+                write(fd, "\n", 1);
+            }
+            free(keyword);
+        }
+        close (fd);
+        exit(0); 
+    }
+
+    else if (pid > 0) // Aqui estamos en el padre
+	{
+		pid = waitpid(-1, NULL, 0);
+    }
+    return (0);
 }
